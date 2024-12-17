c7d41f426a21dc359bab6c6f023a1dcb4b6dc970
Dane Cross <dane.cross2016@gmail.com>
2024-12-16 17:42:20 -0800

    start ggl combinations branch

--- scripts/testing_ggl_correlations.py
+++ scripts/testing_ggl_correlations.py
@@ -1,4 +1,4 @@
-# For Dane and Yunhao's investigation:
+# For Dane, Yunhao, and Leonel's investigation:
 # make scripts for testing GGL correlations here
 # then make a notebook that imports these scripts and runs them
 
